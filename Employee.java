c7325bf22aab0237e41f49f204b240885621d0b4
nruffier <n.ruffier74@gmail.com>
2019-11-13 12:41:47 +0100

    Ajout de variable et de constructeur par fefaut de la classe employee

--- Employee.java
+++ Employee.java
@@ -1,4 +1,15 @@
 public class Employee{
 	
 	private String nom;
+	private String prenom;
+	private String mail;
+	private String telephone;
+	private String adresse;
+
+	/*
+	 * ctor
+	*/
+	public Employee() {
+	}
+
 }
